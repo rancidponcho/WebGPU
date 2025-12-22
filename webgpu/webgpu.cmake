c9df740c43da7cf0987ab3f0d2365c109580f1ba
rancidponcho <85041596+rancidponcho@users.noreply.github.com>
2025-12-22 14:58:49 -0600

    Update webgpu.cmake
    testing new wgpu fetcher

--- webgpu/webgpu.cmake
+++ webgpu/webgpu.cmake
@@ -1,93 +1,41 @@
-# This file is part of the "Learn WebGPU for C++" book.
-#   https://eliemichel.github.io/LearnWebGPU
-# 
-# MIT License
-# Copyright (c) 2022-2024 Elie Michel and the wgpu-native authors
-# 
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-# 
-# The above copyright notice and this permission notice shall be included in all
-# copies or substantial portions of the Software.
-# 
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-# SOFTWARE.
 
+cmake_minimum_required(VERSION 3.24) # 3.24+ adds better FetchContent support
+project(MyWebGPUProject)
+
+# --- Dawn Build Configuration ---
+# These MUST be set before FetchContent_MakeAvailable to override Dawn's defaults
+set(DAWN_FETCH_DEPENDENCIES ON CACHE BOOL "" FORCE)
+set(DAWN_BUILD_PROTOBUF OFF CACHE BOOL "" FORCE)
+set(DAWN_BUILD_SAMPLES OFF CACHE BOOL "" FORCE)
+set(DAWN_BUILD_NODE_BINDINGS OFF CACHE BOOL "" FORCE)
+set(TINT_BUILD_FUZZERS OFF CACHE BOOL "" FORCE)
+set(TINT_BUILD_AST_FUZZER OFF CACHE BOOL "" FORCE)
+set(TINT_BUILD_IR_FUZZER OFF CACHE BOOL "" FORCE)
+set(TINT_BUILD_TESTS OFF CACHE BOOL "" FORCE)
+set(TINT_BUILD_BENCHMARKS OFF CACHE BOOL "" FORCE)
+
+# Use standard FetchContent (no more deprecated manual download macros)
 include(FetchContent)
 
-set(WEBGPU_BACKEND "WGPU" CACHE STRING "Backend implementation of WebGPU. Possible values are EMSCRIPTEN, WGPU, WGPU_STATIC and DAWN (it does not matter when using emcmake)")
+FetchContent_Declare(
+    dawn
+    GIT_REPOSITORY https://dawn.googlesource.com/dawn
+    GIT_TAG        chromium/6842 # Using a stable Chromium-branch tag is safer than 'main'
+    GIT_SHALLOW    ON
+)
 
-# FetchContent's GIT_SHALLOW option is buggy and does not actually do a shallow
-# clone. This macro takes care of it.
-macro(FetchContent_DeclareShallowGit Name GIT_REPOSITORY GitRepository GIT_TAG GitTag)
-	FetchContent_Declare(
-		"${Name}"
+# This replaces the old "MakeAvailable" logic and applies our flags
+FetchContent_MakeAvailable(dawn)
 
-		# This is what it'd look line if GIT_SHALLOW was indeed working:
-		#GIT_REPOSITORY "${GitRepository}"
-		#GIT_TAG        "${GitTag}"
-		#GIT_SHALLOW    ON
+# --- Your Application ---
+add_executable(${PROJECT_NAME} main.cpp)
 
-		# Manual download mode instead:
-		DOWNLOAD_COMMAND
-			cd "${FETCHCONTENT_BASE_DIR}/${Name}-src" &&
-			git init &&
-			git fetch --depth=1 "${GitRepository}" "${GitTag}" &&
-			git reset --hard FETCH_HEAD
-	)
-endmacro()
+# Link against dawn::webgpu_cpp (the C++ wrapper) or dawn::dawn_native
+# Note: Dawn's targets are namespaced
+target_link_libraries(${PROJECT_NAME} PRIVATE dawn::webgpu_cpp dawn::dawn_native)
 
-if (NOT TARGET webgpu)
-	string(TOUPPER ${WEBGPU_BACKEND} WEBGPU_BACKEND_U)
-
-	if (EMSCRIPTEN OR WEBGPU_BACKEND_U STREQUAL "EMSCRIPTEN")
-
-		FetchContent_DeclareShallowGit(
-			webgpu-backend-emscripten
-			GIT_REPOSITORY https://github.com/eliemichel/WebGPU-distribution
-			GIT_TAG        emscripten-v3.1.61
-		)
-		FetchContent_MakeAvailable(webgpu-backend-emscripten)
-
-	elseif (WEBGPU_BACKEND_U STREQUAL "WGPU")
-
-		FetchContent_DeclareShallowGit(
-			webgpu-backend-wgpu
-			GIT_REPOSITORY https://github.com/eliemichel/WebGPU-distribution
-			GIT_TAG        0ccff38289d39ff0113a69325a33b341a04b43d7 # wgpu-v0.19.4.1 + fix
-		)
-		FetchContent_MakeAvailable(webgpu-backend-wgpu)
-
-	elseif (WEBGPU_BACKEND_U STREQUAL "WGPU_STATIC")
-
-		FetchContent_DeclareShallowGit(
-			webgpu-backend-wgpu
-			GIT_REPOSITORY https://github.com/eliemichel/WebGPU-distribution
-			GIT_TAG        wgpu-static-v0.19.4.1
-		)
-		FetchContent_MakeAvailable(webgpu-backend-wgpu)
-
-	elseif (WEBGPU_BACKEND_U STREQUAL "DAWN")
-
-		FetchContent_DeclareShallowGit(
-			webgpu-backend-dawn
-			GIT_REPOSITORY https://github.com/eliemichel/WebGPU-distribution
-			GIT_TAG        853fe9a472ad92f3323938c3c6bfdfedaed164ee # dawn-6512 + emscripten-v3.1.61
-		)
-		FetchContent_MakeAvailable(webgpu-backend-dawn)
-
-	else()
-
-		message(FATAL_ERROR "Invalid value for WEBGPU_BACKEND: possible values are EMSCRIPTEN, WGPU, WGPU_STATIC and DAWN, but '${WEBGPU_BACKEND_U}' was provided.")
-
-	endif()
-endif()
+# Set C++ standard (Dawn requires C++17 or 20)
+set_target_properties(${PROJECT_NAME} PROPERTIES
+    CXX_STANDARD 17
+    CXX_STANDARD_REQUIRED ON
+)
